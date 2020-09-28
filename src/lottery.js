e6ec7f119a0f4fa635ee5be80cade496a8a48572
Adrian Asagba <adrian.asagba@ctt.co.uk>
2020-09-28 13:26:50 +0100

    add files from course

---
+++ src/lottery.js
@@ -0,0 +1,8 @@
+import web3 from './web3';
+
+const address = '0x0713D4415E278Bb48425220EB684835FBc63CdFf';
+// copy of contract instance on blockchain, copied from CL
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+// export copy of contract
+export default new web3.eth.Contract(abi, address);
